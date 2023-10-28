3009ed7f85dae8642894051b595de7342d0f78b6
Allen Pestaluky <allenpestaluky@gmail.com>
2023-10-28 12:28:55 -0400

    Added export properties, etc.

--- node_log.gd
+++ node_log.gd
@@ -1,17 +1,37 @@
 class_name NodeLog extends Node
 
 static var init_count: int = 0
+static var static_member: int = 42
+
 var self_init_number: int = 0
+
 var process_count: int = 0
 var physics_process_count: int = 0
+var draw_count: int = 0
+
+var member: int = 42
+var property_member: int = 42:
+	get:
+		print(name, ": property_member: get")
+		return property_member
+	set(value):
+		print(name, ": property_member: set(", value, ")")
+		property_member = value
+@export var export_member: int = 42:
+	get:
+		print(name, ": export_member: get")
+		return export_member
+	set(value):
+		print(name, ": export_member: set(", value, ")")
+		export_member = value
 
 func _init() -> void:
 	init_count += 1
 	self_init_number = init_count
-	print("_init() #", self_init_number)
+	print("_init() #", self_init_number, " (member: ", member, " export_member: ", export_member, ")")
 
 static func _static_init() -> void:
-	print("NodeLog: _static_init()")
+	print("NodeLog: _static_init() (static_member: ", static_member, ")")
 
 func _notification(what: int) -> void:
 	match what:
@@ -53,8 +73,20 @@ func _notification(what: int) -> void:
 			print(name, ": _notification(NOTIFICATION_WM_MOUSE_ENTER)")
 		NOTIFICATION_WM_MOUSE_EXIT:
 			print(name, ": _notification(NOTIFICATION_WM_MOUSE_EXIT)")
-		41:
+		NOTIFICATION_NODE_RECACHE_REQUESTED:
+			print(name, ": _notification(NOTIFICATION_NODE_RECACHE_REQUESTED)")
+		NOTIFICATION_WM_CLOSE_REQUEST:
+			print(name, ": _notification(NOTIFICATION_WM_CLOSE_REQUEST)")
+		Node2D.NOTIFICATION_DRAW:
+			print(name, ": _notification(NOTIFICATION_DRAW)")
+		Node2D.NOTIFICATION_ENTER_CANVAS:
+			print(name, ": _notification(NOTIFICATION_ENTER_CANVAS)")
+		Node2D.NOTIFICATION_VISIBILITY_CHANGED:
+			print(name, ": _notification(NOTIFICATION_VISIBILITY_CHANGED)")
+		Node3D.NOTIFICATION_ENTER_WORLD:
 			print(name, ": _notification(NOTIFICATION_ENTER_WORLD (Node3D) or NOTIFICATION_MOUSE_ENTER (Control))")
+		2000:
+			print(name, ": _notification(NOTIFICATION_TRANSFORM_CHANGED)")
 		_:
 			print(name, ": _notification(", what, ")")
 
@@ -71,22 +103,27 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	print(name, ": _unhandled_key_input(", event.as_text(), ")")
 
 func _enter_tree() -> void:
-	print(name, ": _enter_tree()")
+	print(name, ": _enter_tree() init_number: ", self_init_number)
 
 func _exit_tree() -> void:
 	print(name, ": _exit_tree()")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(name, ": _ready() init_number: ", self_init_number)
+	print(name, ": _ready()")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	process_count += 1
-	if process_count < 5:
-		print(name, " (init_number ", self_init_number, "): _process()")
+	if process_count < 3:
+		process_count += 1
+		print(name, ": _process()")
 
-func _physics_process(delta: float) -> void:
-	physics_process_count += 1
-	if physics_process_count < 5:
-		print(name, " (init_number ", self_init_number, "): _physics_process()")
+func _physics_process(_delta: float) -> void:
+	if physics_process_count < 3:
+		physics_process_count += 1
+		print(name, ": _physics_process()")
+
+func _draw() -> void:
+	if draw_count < 3:
+		draw_count += 1
+		print(name, ": _draw()")
